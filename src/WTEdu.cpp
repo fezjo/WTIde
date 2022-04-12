bbe049790d45c32d5448ed236d07dfe3163a85b8
Jozef Číž <jozef.ciz.ml@gmail.com>
2022-04-12 22:59:40 +0200

    Refactor Window to App and move control from main to App

--- src/WTEdu.cpp
+++ src/WTEdu.cpp
@@ -67,7 +67,7 @@ int main(int, char**)
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
     //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
     io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 
@@ -98,12 +98,8 @@ int main(int, char**)
     ImWchar glyph_ranges[] = { 1, 0x10FFFF, 0 };
     io.Fonts->AddFontFromFileTTF(fontPath.string().c_str(), 16.0, NULL, glyph_ranges);
 
-    // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-    Window my_window;
+    App app;
+    app.show_demo_window = true;
 
     // Main loop
     bool done = false;
@@ -123,57 +119,20 @@ int main(int, char**)
             if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(window))
                 done = true;
         }
-
         // Start the Dear ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
         ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
 
-        my_window.init();
-        my_window.update();
-        my_window.show(nullptr);
-
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
-
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
+        app.init();
+        app.update();
+        app.show(nullptr);
 
         // Rendering
         ImGui::Render();
         glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
         glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
@@ -181,7 +140,7 @@ int main(int, char**)
     }
 
     // Cleanup
-    my_window.destroy();
+    app.destroy();
 
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplSDL2_Shutdown();
--- src/app.cpp
+++ src/app.cpp
@@ -5,7 +5,11 @@
 #include "plugins/editor.h"
 #include "plugins/filetree.h"
 
-class Window {
+class App {
+public:
+    bool show_demo_window = false;
+
+protected:
     bool initialized = false;
     ImGuiWindowFlags flags;
     std::vector<ZepWrapper*> zepWrappers;
@@ -13,9 +17,9 @@ class Window {
     std::vector<IPlugin*> plugins; 
 
 public:
-    Window() = default;
+    App() = default;
 
-    Window& get() {
+    App& get() {
     }
 
     void init() {
@@ -34,7 +38,7 @@ public:
         openEditor(fs::path("..") / "src" / "editor.cpp");
         zepWrappers[1]->GetEditor().SetGlobalMode(Zep::ZepMode_Standard::StaticName());
 
-        ft = new FileTree(std::bind(&Window::openEditor, this, std::placeholders::_1));
+        ft = new FileTree(std::bind(&App::openEditor, this, std::placeholders::_1));
         ft->displaySize = ImVec2(120, 640);
         ft->setPath(fs::path("."));
         plugins.push_back(ft);
@@ -46,15 +50,44 @@ public:
     }
 
     void show(bool *p_open) {
+        static ImVec4 clear_color = ImVec4(0.06f, 0.06f, 0.06f, 1.00f);
+
         // Just show it
         bool use_work_area = true;
         const ImGuiViewport* viewport = ImGui::GetMainViewport();
         ImGui::SetNextWindowPos(use_work_area ? viewport->WorkPos : viewport->Pos);
         ImGui::SetNextWindowSize(use_work_area ? viewport->WorkSize : viewport->Size);
 
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        ImGui::PushStyleColor(ImGuiCol_WindowBg, clear_color);
         if (!ImGui::Begin("Main", p_open, flags)) {
+            ImGui::PopStyleColor();
             ImGui::End();
             return;
+        }
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Debug");                                  // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo App", &show_demo_window);      // Edit bools storing our window open/close state
+
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
         }
 
         for(auto p: plugins) {
@@ -62,6 +95,7 @@ public:
             if (!p->alive)
                 destroy_plugin(p);
         }
+        ImGui::PopStyleColor();
         ImGui::End();
 
     }
