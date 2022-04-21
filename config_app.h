f45bfb8ea05d2f4092a8e151ab441ff8c7527e9a
Jozef Číž <jozef.ciz.ml@gmail.com>
2022-04-21 20:24:42 +0200

    Add WTStar submodule

--- config_app.h
+++ config_app.h
@@ -1,4 +1,4 @@
 /* #undef APPLICATION_NAME */
 /* #undef APPLICATION_VERSION */
-#define APP_ROOT "/home/fezjo/matfyz/bakalarka/wtedu2"
+#define APP_ROOT "/home/fezjo/matfyz/bakalarka/wtedu"
 /* #undef ZEP_SINGLE_HEADER */
