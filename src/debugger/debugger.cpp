d3e9af52611d858806e86ddc772e681d1e770f08
Jozef Číž <jozef.ciz.ml@gmail.com>
2022-04-25 21:10:13 +0200

    Add step out

--- src/debugger/debugger.cpp
+++ src/debugger/debugger.cpp
@@ -210,10 +210,13 @@ int Debugger::stepOver() {
         if (resp != -11)
             break;
         uint32_t current_level = STACK_SIZE(env->frames, WTStar::frame_t *);
+        std::cerr << "stepOver resp " << resp << " level " << current_level << "[" << initial_level
+                  << "]" << std::endl;
         if (current_level <= initial_level)
             break;
     }
-    std::cerr << "stepOver execute stopped with resp " << resp << std::endl;
+    std::cerr << "stepOver execute stopped with resp " << resp << " on line " << env->pc
+              << std::endl;
     return resp;
 }
 
@@ -221,11 +224,31 @@ int Debugger::stepInto() {
     if (!env)
         return -2;
     int resp = WTStar::execute(env, -1, 0, 2 | stop_on_bp);
-    std::cerr << "stepInto execute stopped with resp " << resp << std::endl;
+    uint32_t current_level = STACK_SIZE(env->frames, WTStar::frame_t *);
+    std::cerr << "stepInto execute stopped with resp " << resp << " on line " << env->pc
+              << " with level " << current_level << std::endl;
     return resp;
 }
 
-int Debugger::stepOut() { std::cerr << "NOT IMPLEMENTED" << std::endl; }
+int Debugger::stepOut() {
+    if (!env)
+        return -2;
+    uint32_t initial_level = STACK_SIZE(env->frames, WTStar::frame_t *);
+    int resp;
+    while (true) {
+        resp = WTStar::execute(env, -1, 0, 4 | stop_on_bp);
+        if (resp != -11)
+            break;
+        uint32_t current_level = STACK_SIZE(env->frames, WTStar::frame_t *);
+        std::cerr << "stepOut resp " << resp << " level " << current_level << "[" << initial_level
+                  << "]" << std::endl;
+        if (current_level < initial_level)
+            break;
+    }
+    std::cerr << "stepOut execute stopped with resp " << resp << " on line " << env->pc
+              << std::endl;
+    return resp;
+}
 
 bool Debugger::setBreakpoint(const std::string &file, int line) {
     return setBreakpointWithCondition(file, line, "");
