ba44c65c04ac9f3eb0cb1ef44171d0a28592e3a4
Jozef Číž <jozef.ciz.ml@gmail.com>
2022-04-25 21:16:55 +0200

    Debugger control plugin consistent response to execution, string input

--- src/plugins/debugger_control_plugin.cpp
+++ src/plugins/debugger_control_plugin.cpp
@@ -32,47 +32,49 @@ void DebuggerControlPlugin::show() {
     if (ImGui::IsItemEdited() || !seen)
         source_fn_color = ImGui::GetStyleColorVec4(ImGuiCol_FrameBg);
 
-    if (ImGui::Button("Compile")) {
-        debugger->clearCompilationOutput();
-        debugger->compile();
-        callbacks["refresh_analyzer"](0);
-        callbacks["set_compilation_output"](debugger->getCompilationOutput());
-    }
-    ImGui::SameLine();
-    if (ImGui::Button("Run")) {
-        callbacks["set_input"](0);
-        if (debugger->runExecution() == -1)
-            callbacks["set_output"](debugger->getOutput());
-    }
-    ImGui::SameLine();
-    if (ImGui::Button("Stop")) {
-        debugger->stopExecution();
-    }
+    {
+        int resp = -1000;
+        if (ImGui::Button("Compile")) {
+            debugger->clearCompilationOutput();
+            debugger->compile();
+            callbacks["refresh_analyzer"](0);
+            callbacks["set_compilation_output"](debugger->getCompilationOutput());
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Run")) {
+            callbacks["set_input"](0);
+            resp = debugger->runExecution();
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Stop")) {
+            debugger->stopExecution();
+        }
 
-    if (ImGui::Button("Continue")) {
-        if (debugger->continueExecution() == -1)
-            callbacks["set_output"](debugger->getOutput());
-    }
-    ImGui::SameLine();
-    ImGui::BeginDisabled(true);
-    if (ImGui::Button("Pause")) {
-        debugger->pauseExecution();
-    }
-    ImGui::EndDisabled();
+        if (ImGui::Button("Continue")) {
+            resp = debugger->continueExecution();
+        }
+        ImGui::SameLine();
+        ImGui::BeginDisabled(true);
+        if (ImGui::Button("Pause")) {
+            debugger->pauseExecution();
+        }
+        ImGui::EndDisabled();
 
-    ImGui::Text("Step");
-    ImGui::SameLine();
-    if (ImGui::Button("Over")) {
-        debugger->stepOver();
-    }
-    ImGui::SameLine();
-    if (ImGui::Button("Into")) {
-        if (debugger->stepInto() == -1)
+        ImGui::Text("Step");
+        ImGui::SameLine();
+        if (ImGui::Button("Over")) {
+            resp = debugger->stepOver();
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Into")) {
+            resp = debugger->stepInto();
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Out")) {
+            resp = debugger->stepOut();
+        }
+        if (resp == -1)
             callbacks["set_output"](debugger->getOutput());
-    }
-    ImGui::SameLine();
-    if (ImGui::Button("Out")) {
-        debugger->stepOut();
     }
 
     if (ImGui::Button("Breakpoints")) {
@@ -81,7 +83,7 @@ void DebuggerControlPlugin::show() {
     ImGui::SameLine();
     ImGui::Checkbox("Stop on BP", &debugger->stop_on_bp);
 
-    static char fileBuffer[256] = "";
+    static std::string fileBuffer = "";
     static int lineBuffer = 0;
     if (ImGui::BeginPopup("Breakpoints")) {
         ImGui::BeginDisabled(true);
@@ -89,7 +91,7 @@ void DebuggerControlPlugin::show() {
             ImGui::OpenPopup("Add Breakpoint");
         }
         if (ImGui::BeginPopup("Add Breakpoint")) {
-            ImGui::InputText("File", fileBuffer, sizeof(fileBuffer));
+            ImGui::InputText("File", &fileBuffer);
             ImGui::InputInt("Line", &lineBuffer);
             if (ImGui::Button("Add")) {
                 debugger->setBreakpoint(fileBuffer, lineBuffer);
@@ -101,7 +103,7 @@ void DebuggerControlPlugin::show() {
             ImGui::OpenPopup("Remove Breakpoint");
         }
         if (ImGui::BeginPopup("Remove Breakpoint")) {
-            ImGui::InputText("File", fileBuffer, sizeof(fileBuffer));
+            ImGui::InputText("File", &fileBuffer);
             ImGui::InputInt("Line", &lineBuffer);
             if (ImGui::Button("Remove")) {
                 debugger->removeBreakpoint(fileBuffer, lineBuffer);
